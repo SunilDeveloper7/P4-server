c3eafb54336f945d615eca3faf207b5fa214ac27
SunilDeveloper7 <sunnil474@gmail.com>
2021-04-15 16:39:50 -0700

    Auth completed and tested with postman but may be at the end i might change the auth steps if i have time

--- resolvers/users.js
+++ resolvers/users.js
@@ -5,40 +5,101 @@
     const User = require('../models/User');
 
 
-module.exports = {
-    Mutation: {
-        async register(
-            _,
+    const {
+        validateRegisterInput,
+        validateLoginInput
+        } = require('../auth/validators');
+        
+        
+        function generateToken(user) {
+            return jwt.sign(
             {
-                registerInput:{ username,email,password,confirmPassword}
+                id: user.id,
+                email: user.email,
+                username: user.username
             },
-            context,
-            info
-        ) {
+            SECRET_KEY,
+            { expiresIn: '1h' }
+            );
+        }
+        
+        module.exports = {
+            Mutation: {
+            async login(_, { username, password }) {
+                const { errors, valid } = validateLoginInput(username, password);
+        
+                if (!valid) {
+                throw new UserInputError('Errors', { errors });
+                }
+        
+                const user = await User.findOne({ username });
+        
+                if (!user) {
+                errors.general = 'User not found';
+                throw new UserInputError('NO USER FOUND ON OUR DATABASE', { errors });
+                }
+        
+                const match = await bcrypt.compare(password, user.password);
+                if (!match) {
+                errors.general = 'Wrong INFO,TRY AGAIN';
+                throw new UserInputError('Wrong INFO,TRY AGAIN', { errors });
+                }
+        
+                const token = generateToken(user);
+        
+                return {
+                ...user._doc,
+                id: user._id,
+                token
+                };
+            },
+            async register(
+                _,
+                {
+                registerInput: { username, email, password, confirmPassword }
+                }
+            ) {
+                // Validate user data
+                const { valid, errors } = validateRegisterInput(
+                username,
+                email,
+                password,
+                confirmPassword
+                );
+                if (!valid) {
+                throw new UserInputError('Errors', { errors });
+                }
+                // TODO: Make sure user doesnt already exist
+                const user = await User.findOne({ username });
+                if (user) {
+                throw new UserInputError('Username is already taken', {
+                    errors: {
+                    username: 'This username is taken,Sorry'
+                    }
+                });
+                }
+                // hash password and create an auth token
             const hash = await bcrypt.hash(password, 12);
-
-            const newUser = new User({
+        
+                const newUser = new User({
                 email,
                 username,
-                password: hash,
+                password:hash,
                 createdAt: new Date().toISOString()
-            });
-            const res = await newUser.save()
-            const token = jwt.sign({
-                id:res.id,
-                email:res.email,
-                username:res.username
-            }, SECRET_KEY, {expiresIn:'1h'});
-
-            return {
+                });
+        
+                const res = await newUser.save();
+        
+                const token = generateToken(res);
+        
+                return {
                 ...res._doc,
-                id:res.id,
+                id: res._id,
                 token
+                };
             }
-
-        }
-    }
-}
+            }
+        };
 
 
 
@@ -58,98 +119,59 @@ module.exports = {
 
 
 
+// module.exports = {
+//     Mutation: {
+//         async register(
+//             _,
+//             {
+//                 registerInput:{ username,email,password,confirmPassword}
+//             },
+//             context,
+//             info
+//         ) {
+//             const hash = await bcrypt.hash(password, 12);
 
-    // const {
-    // validateRegisterInput,
-    // validateLoginInput 
-    // } = require('../auth/validators.js');
+//             const newUser = new User({
+//                 email,
+//                 username,
+//                 password: hash,
+//                 createdAt: new Date().toISOString()
+//             });
+//             const res = await newUser.save()
+//             const token = jwt.sign({
+//                 id:res.id,
+//                 email:res.email,
+//                 username:res.username
+//             }, SECRET_KEY, {expiresIn:'1h'});
 
-    // function generateToken(user) {
-    // return jwt.sign(
-    //     {
-    //     id: user.id,
-    //     email: user.email,
-    //     username: user.username
-    //     },
-    //     SECRET_KEY,
-    //     { expiresIn: '1h' }
-    // );
-    // }
+//             return {
+//                 ...res._doc,
+//                 id:res.id,
+//                 token
+//             }
 
-    // module.exports = {
-    // Mutation: {
-    //     async login(_, { username, password }) {
-    //     const { errors, valid } = validateLoginInput(username, password);
+//         }
+//     }
 
-    //     if (!valid) {
-    //         throw new UserInputError('Errors', { errors });
-    //     }
 
-    //     const user = await User.findOne({ username });
+    
+// }
 
-    //     if (!user) {
-    //         errors.general = 'User not found';
-    //         throw new UserInputError('User not found', { errors });
-    //     }
 
-    //     const match = await bcrypt.compare(password, user.password);
-    //     if (!match) {
-    //         errors.general = 'Wrong crendetials';
-    //         throw new UserInputError('Wrong crendetials', { errors });
-    //     }
 
-    //     const token = generateToken(user);
 
-    //     return {
-    //         ...user._doc,
-    //         id: user._id,
-    //         token
-    //     };
-    //     },
-    //     async register(
-    //     _,
-    //     {
-    //         registerInput: { username, email, password, confirmPassword }
-    //     }
-    //     ) {
-    //     // Validate user data
-    //     const { valid, errors } = validateRegisterInput(
-    //         username,
-    //         email,
-    //         password,
-    //         confirmPassword
-    //     );
-    //     if (!valid) {
-    //         throw new UserInputError('Errors', { errors });
-    //     }
-    //     // TODO: Make sure user doesnt already exist
-    //     const user = await User.findOne({ username });
-    //     if (user) {
-    //         throw new UserInputError('Username is taken', {
-    //         errors: {
-    //             username: 'This username is taken'
-    //         }
-    //         });
-    //     }
-    //     // hash password and create an auth token
-    //     password = await bcrypt.hash(password, 12);
 
-    //     const newUser = new User({
-    //         email,
-    //         username,
-    //         password,
-    //         createdAt: new Date().toISOString()
-    //     });
 
-    //     const res = await newUser.save();
 
-    //     const token = generateToken(res);
 
-    //     return {
-    //         ...res._doc,   
-    //         id: res._id,
-    //         token
-    //     };
-    //     }
-    // }
-    // };
+
+
+
+
+
+
+
+
+
+
+
