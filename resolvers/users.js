c3e0d917f38f8394f603bf1a488c1b173827274b
SunilDeveloper7 <sunnil474@gmail.com>
2021-04-16 09:57:33 -0700

    CRUD operation add to post

--- resolvers/users.js
+++ resolvers/users.js
@@ -119,47 +119,6 @@
 
 
 
-// module.exports = {
-//     Mutation: {
-//         async register(
-//             _,
-//             {
-//                 registerInput:{ username,email,password,confirmPassword}
-//             },
-//             context,
-//             info
-//         ) {
-//             const hash = await bcrypt.hash(password, 12);
-
-//             const newUser = new User({
-//                 email,
-//                 username,
-//                 password: hash,
-//                 createdAt: new Date().toISOString()
-//             });
-//             const res = await newUser.save()
-//             const token = jwt.sign({
-//                 id:res.id,
-//                 email:res.email,
-//                 username:res.username
-//             }, SECRET_KEY, {expiresIn:'1h'});
-
-//             return {
-//                 ...res._doc,
-//                 id:res.id,
-//                 token
-//             }
-
-//         }
-//     }
-
-
-    
-// }
-
-
-
-
 
 
 
